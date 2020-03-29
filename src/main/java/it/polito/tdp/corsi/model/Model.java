9445977f9b4078efa14ded320ca5b6246f9b0abb
Enrico Zappador <s236559@studenti.polito.it>
2020-03-29 16:56:24 +0200

    Primo punto

--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,7 +1,16 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.List;
+
+import it.polito.tdp.corsi.db.CorsoDAO;
+
 public class Model {
 	
-
+	public List<Corso> getCorsibyPeriodo(Integer pd){
+		CorsoDAO dao = new CorsoDAO(); 
+		return dao.getCorsibyPeriodo(pd); 
+	}
+	
+	
 
 }
---
+++ src/main/java/it/polito/tdp/corsi/model/TestModel.java
@@ -0,0 +1,11 @@
+package it.polito.tdp.corsi.model;
+
+public class TestModel {
+
+	public static void main(String[] args) {
+		Model model = new Model(); 
+		
+		System.out.println(model.getCorsibyPeriodo(1));
+	}
+
+}
